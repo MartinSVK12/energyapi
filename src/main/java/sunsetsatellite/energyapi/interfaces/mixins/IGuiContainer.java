cb896a98f407454b208a6b96ffe51417b6dab080
MartinSVK12 <37455793+martinsvk12@users.noreply.github.com>
2023-04-06 08:25:20 +0200

    Remove unused method.

--- src/main/java/sunsetsatellite/energyapi/interfaces/mixins/IGuiContainer.java
+++ src/main/java/sunsetsatellite/energyapi/interfaces/mixins/IGuiContainer.java
@@ -1,7 +1,5 @@
 package sunsetsatellite.energyapi.interfaces.mixins;
 
-import net.minecraft.src.ItemStack;
 
 public interface IGuiContainer {
-    void drawItemStack(ItemStack stack, int x, int y);
 }
--- src/main/java/sunsetsatellite/energyapi/mixin/GuiContainerMixin.java
+++ src/main/java/sunsetsatellite/energyapi/mixin/GuiContainerMixin.java
@@ -49,23 +49,4 @@ public class GuiContainerMixin extends GuiScreen
         }
     }
 
-    public void drawItemStack(ItemStack stack, int x, int y) {
-        if(stack != null) {
-            GL11.glPushMatrix();
-            GL11.glRotatef(120.0F, 1.0F, 0.0F, 0.0F);
-            RenderHelper.enableStandardItemLighting();
-            GL11.glPopMatrix();
-            GL11.glPushMatrix();
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-            GL11.glTranslatef(0.0F, 0.0F, 32.0F);
-            itemRenderer.renderItemIntoGUI(this.fontRenderer, this.mc.renderEngine, stack, x, y, 1.0F);
-            GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-            RenderHelper.disableStandardItemLighting();
-            GL11.glDisable(GL11.GL_LIGHTING);
-            GL11.glDisable(GL11.GL_DEPTH_TEST);
-            GL11.glPopMatrix();
-        }
-    }
-
 }
